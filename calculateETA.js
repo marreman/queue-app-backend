b159f9965a80257240933a3d9af1bd43b7720256
Martin Liberg <mr.liberg@gmail.com>
2014-08-23 18:12:25 +0200

    calculating eta only on a specific subset of the sessions

--- calculateETA.js
+++ calculateETA.js
@@ -1,43 +1,54 @@
 var Firebase = require('firebase');
 
-var root = new Firebase('https://queue-app.firebaseio.com/')
+var root = new Firebase('https://queue-app-dev.firebaseio.com/')
   , locations = root.child('locations')
   , queueSessions = root.child('queueSessions')
-  , beacons = [];
+  , beacons = []
+  , ONE_HOUR_IN_MILLISECONDS = 60 * 60 * 60 * 1000;
 
 function Beacon(beaconId) {
   this.id = beaconId;
-  queueSessions.child(this.id).on('value', this.onSessionsUpdated, this);
+
+  locations.child(this.id).child('etaHistoryTimeLimit').on('value', function (snap) {
+    this.etaHistoryTimeLimit = snap.val() || ONE_HOUR_IN_MILLISECONDS;
+    queueSessions.child(this.id).off();
+    queueSessions.child(this.id).on('value', this.onSessionsUpdated, this);
+  }, this);
 }
 
 Beacon.prototype.onSessionsUpdated = function (snap) {
   var eta = this.calculateETA(snap.val());
-  locations.child(this.id).child('eta').set(eta)
+  locations.child(this.id).child('eta').set(eta);
 };
 
 Beacon.prototype.calculateETA = function (sessionEntries) {
   var eta
     , sumOfEntries = 0
-    , numberOfEntries = 0;
+    , numberOfEntries = 0
+    , timeLimit = new Date().getTime() - this.etaHistoryTimeLimit;
 
   if (!sessionEntries) {
     return null;
   }
 
-  numberOfEntries = Object.keys(sessionEntries).length;
-
   for (var name in sessionEntries) {
     var session = sessionEntries[name]
-      , diff = session.end - session.start;
+      , queueTime;
 
-    sumOfEntries += diff;
+    if (session.end > timeLimit) {
+      queueTime = session.end - session.start;
+      if (queueTime > 0) {
+        sumOfEntries += queueTime;
+        numberOfEntries++;
+      }
+    }
   }
 
   eta = (sumOfEntries / numberOfEntries).toFixed();
-  return eta;
+  return numberOfEntries ? eta : null;
 };
 
 queueSessions.on('child_added', function (snap) {
-  var beacon = new Beacon(snap.name(), snap.val());
+  var beacon = new Beacon(snap.name());
   beacons.push(beacon);
 });
