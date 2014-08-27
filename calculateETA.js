14f9766f397eac93cc0beaddb5a9a0670edf297b
Martin Liberg <mr.liberg@gmail.com>
2014-08-27 11:02:45 +0200

    calculating current number of females and males

--- calculateETA.js
+++ calculateETA.js
@@ -1,33 +1,30 @@
 var Firebase = require('firebase');
 
-var root = new Firebase('https://queue-app-dev.firebaseio.com/')
+var root = new Firebase('https://queue-app.firebaseio.com/')
   , locations = root.child('locations')
   , queueSessions = root.child('queueSessions')
   , beacons = []
-  , ONE_HOUR_IN_MILLISECONDS = 60 * 60 * 60 * 1000;
+  , TIME_LIMIT = 60 * 60 * 60 * 1000; //ONE HOUR IN MILLISECONDS
 
 function Beacon(beaconId) {
   this.id = beaconId;
-
-  locations.child(this.id).child('etaHistoryTimeLimit').on('value', function (snap) {
-    this.etaHistoryTimeLimit = snap.val() || ONE_HOUR_IN_MILLISECONDS;
-    queueSessions.child(this.id).off();
-    queueSessions.child(this.id).on('value', this.onSessionsUpdated, this);
-  }, this);
+  this.fbRef = queueSessions.child(this.id);
+  this.fbRef.off();
+  this.fbRef.on('value', this.onSessionsUpdated, this);
 }
 
 Beacon.prototype.onSessionsUpdated = function (snap) {
-  var estimatedQueueTime = this.calculateETA(snap.val());
-  locations
-    .child(this.id + '/currentStatus/estimatedQueueTime')
-    .set(estimatedQueueTime);
+  var currentStatus = this.calculateCurrentStatus(snap.val());
+  locations.child(this.id + '/currentStatus').set(currentStatus);
 };
 
-Beacon.prototype.calculateETA = function (sessionEntries) {
-  var eta
+Beacon.prototype.calculateCurrentStatus = function (sessionEntries) {
+  var estimatedQueueTime
     , sumOfEntries = 0
     , numberOfEntries = 0
-    , timeLimit = new Date().getTime() - this.etaHistoryTimeLimit;
+    , numberOfFemales = 0
+    , numberOfMales = 0
+    , timeLimit = new Date().getTime() - TIME_LIMIT;
 
   if (!sessionEntries) {
     return null;
@@ -39,15 +36,23 @@ Beacon.prototype.calculateETA = function (sessionEntries) {
 
     if (session.end > timeLimit) {
       queueTime = session.end - session.start;
+
       if (queueTime > 0) {
         sumOfEntries += queueTime;
         numberOfEntries++;
+        if (session.gender === 'male') numberOfMales++;
+        else if (session.gender === 'female') numberOfFemales++;
       }
     }
   }
 
-  eta = (sumOfEntries / numberOfEntries).toFixed();
-  return numberOfEntries ? eta : null;
+  estimatedQueueTime = (sumOfEntries / numberOfEntries).toFixed();
+
+  return {
+    estimatedQueueTime: numberOfEntries ? estimatedQueueTime : null,
+    numberOfFemales: numberOfMales,
+    numberOfMales: numberOfMales
+  };
 };
 
 queueSessions.on('child_added', function (snap) {
